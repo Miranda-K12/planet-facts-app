69016b06868aafe82e04f0c3541bb2a9997347af
Miranda Kachlavashvili <136162510+Miranda-K12@users.noreply.github.com>
2025-05-06 17:49:18 +0400

    Add responsive styles

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,24 +1,45 @@
 import { Link } from "react-router-dom";
 import data from "../../../starter-code/data.json";
 import styles from "./Header.module.scss";
-
+import { planetColors } from "../../constants/planetColors";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import { useState } from "react";
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const handleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
     <div className={styles.header}>
       <h1 className={styles.header_title}>The Planets</h1>
       <nav>
-        <ul>
+        <ul className={`${styles.navList} ${isMenuOpen ? styles.open : ""}`}>
           {data.map((planet) => (
             <li key={planet.name}>
               <Link
                 to={`${planet.name.toLowerCase()}`}
                 className={styles.navLink}
+                onClick={() => setIsMenuOpen(false)}
               >
+                <span
+                  className={styles.planetCircle}
+                  style={{
+                    backgroundColor:
+                      planetColors[
+                        planet.name.toLowerCase() as keyof typeof planetColors
+                      ],
+                  }}
+                ></span>
+
                 {planet.name}
               </Link>
             </li>
           ))}
         </ul>
+        <i
+          className={`fa-solid fa-bars ${styles.burgerMenu}`}
+          onClick={handleMenu}
+        ></i>
       </nav>
     </div>
   );
